ca80ec6130f07208c08b93a3c820048e0178fa92
Tobi <arotimi.tobiloba@gmail.com>
2022-05-25 10:27:39 +0100

    removed console.log()

--- src/components/index.js
+++ src/components/index.js
@@ -1,15 +1,14 @@
 const importedImages = {
-    ecommerce: require('../images/ecommerce.png'),
-    calculator: require('../images/calculator.png'),
-    form: require('../images/form.png'),
-    fylo: require('../images/fylo.png'),
-    tip: require('../images/tip.png'),
-    restapi: require('../images/restapi.png'),
-   portfolio: require('../images/portfolio.png'),
-   dashboard: require('../images/dashboard.png'),
-   airbnb: require('../images/airbnb.png'),
-   launch: require('../images/launch.png'),
-}
-console.log(importedImages)
+  ecommerce: require("../images/ecommerce.png"),
+  calculator: require("../images/calculator.png"),
+  form: require("../images/form.png"),
+  fylo: require("../images/fylo.png"),
+  tip: require("../images/tip.png"),
+  restapi: require("../images/restapi.png"),
+  portfolio: require("../images/portfolio.png"),
+  dashboard: require("../images/dashboard.png"),
+  airbnb: require("../images/airbnb.png"),
+  launch: require("../images/launch.png"),
+};
 
 export default importedImages;
