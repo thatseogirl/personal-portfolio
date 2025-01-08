3ba1f6d7512226486476b2f9842751cc4fa0aebb
S.M <sm@MacBook-Pro.local>
2025-01-08 23:39:22 +0100

    added-stack-section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,23 +4,10 @@ export default function Header() {
   return (
     <header data-aos="fade-left">
       <div className="flex__container">
-        {/* <section className="flex__container__1">
-          <aside className="flex__container__1__border">
-            <hr className="linerule" />
-            <p className="linerule__text">Tobiloba </p>
-          </aside>
-          </section> */}
           <div className="about">
-            <p className="about">Hello, I'm Tobi.</p>
-            <p className="about_2">A full-stack developer</p>
-            {/* <p data-aos="flip-up" className="about flip">
-            A full-stack developer
-            </p> */}
+            <p >Hello, I'm Tobi.</p>
+            <p >A full-stack developer</p>
           </div>
-    
-        {/* <div className="flex__container__1__border__2">
-          <DesignSvg className="img" />
-        </div> */}
       </div>
      
     </header>
